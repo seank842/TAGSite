efa66decbb3404893ce0dde855e8aee43f1ee856
seank842 <seank842@live.co.uk>
2017-01-23 00:00:37 +0000

    added gsap locally changed links

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>The Adventurer's Guild</title>
+<meta charset="utf-8" />
+<meta name="application-name" content="The Adventurer's Guild" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="author" content="Basement Studios" />
+<!--Get Someone to write a description and Keywords-->
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="generator" content="Visual Studio 2017" />
+<link href="Content/bootstrap.min.css" rel="stylesheet" />
+<link href="Content/css/main.css" rel="stylesheet" />
+<link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    rel="stylesheet" type="text/css" />
+<script src="Scripts/jquery-3.1.1.min.js"></script>
+<script src="Scripts/bootstrap.min.js"></script>
+<script src="Scripts/js_ajax/helper.js"></script>
+<script src="Scripts/js_ajax/loginHelper.js"></script>
+<script src="Scripts/jquery-ui.js"></script>
+<script src="Scripts/js_ajax/charaCreHelper.js"></script>
+<script src="Scripts/js_ajax/charListHelper.js"></script>
+<script src="Scripts/js_ajax/shopHelper.js"></script>
+<script src="Scripts/js_ajax/inventHelper.js"></script>
+
+<!--Load in gsap base-->
+    <script src="Scripts/gsap/TweenMax.min.js"></script>
+    <!--Load in Content specific gsap plugins/utils-->
+    <script src="Scripts/gsap/plugins/ThrowPropsPlugin.min.js"></script>
+    <script src="Scripts/gsap/utils/Draggable.min.js"></script>
+</head>
+<body>
+<div class="navbar" style="background-color: white !important; border-radius:0px"> </div>
+<div id="mBody" class="container"> 
+  <!--Content will be dynamically loaded here--> 
+  
+</div>
+</body>
+=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -47,4 +88,5 @@
   
 </div>
 </body>
+>>>>>>> c7ed5c45516bc41005aa3888b9a1bbe6201c14d9
 </html>
