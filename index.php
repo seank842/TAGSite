08ef7c636aa1a14139daaf1c17a0b131cbdedf07
seank842 <seank842@live.co.uk>
2017-01-23 00:15:12 +0000

    reset after gsap locally didn't work

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,60 +25,11 @@
 <script src="Scripts/js_ajax/inventHelper.js"></script>
 
 <!--Load in gsap base-->
-    <script src="Scripts/gsap/TweenMax.min.js"></script>
-    <!--Load in Content specific gsap plugins/utils-->
-    <script src="Scripts/gsap/plugins/ThrowPropsPlugin.min.js"></script>
-    <script src="Scripts/gsap/utils/Draggable.min.js"></script>
-</head>
-<body>
-<div class="navbar" style="background-color: white !important; border-radius:0px"> </div>
-<div id="mBody" class="container"> 
-  <!--Content will be dynamically loaded here--> 
-  
-</div>
-</body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>The Adventurer's Guild</title>
-<meta charset="utf-8" />
-<meta name="application-name" content="The Adventurer's Guild" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta name="author" content="Basement Studios" />
-<!--Get Someone to write a description and Keywords-->
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta name="generator" content="Visual Studio 2017" />
-<link href="Content/bootstrap.min.css" rel="stylesheet" />
-<link href="Content/css/main.css" rel="stylesheet" />
-<link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    rel="stylesheet" type="text/css" />
-
-<!-- FavIcon -->
-
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="theme-color" content="#ffffff">
-
-<!-- End of favicon -->
-<script src="Scripts/jquery-3.1.1.min.js"></script>
-<script src="Scripts/bootstrap.min.js"></script>
-<script src="Scripts/js_ajax/helper.js"></script>
-<script src="Scripts/js_ajax/loginHelper.js"></script>
-<script src="Scripts/jquery-ui.js"></script>
-<script src="Scripts/js_ajax/charaCreHelper.js"></script>
-<script src="Scripts/js_ajax/charListHelper.js"></script>
-<script src="Scripts/js_ajax/shopHelper.js"></script>
-
-<!--Load in gsap base-->
     <script src="https://selene.hud.ac.uk/js/gsap/TweenMax.min.js"></script>
     <!--Load in Content specific gsap plugins/utils-->
     <script src="https://selene.hud.ac.uk/js/gsap/plugins/ThrowPropsPlugin.min.js"></script>
     <script src="https://selene.hud.ac.uk/js/gsap/utils/Draggable.min.js"></script>
+    <script src="Scripts/js_ajax/inventHelper.js"></script>
 </head>
 <body>
 <div class="navbar" style="background-color: white !important; border-radius:0px"> </div>
@@ -88,5 +38,4 @@
   
 </div>
 </body>
->>>>>>> c7ed5c45516bc41005aa3888b9a1bbe6201c14d9
 </html>
