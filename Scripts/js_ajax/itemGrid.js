68af46bca276d195beefaa9c8915614b543acd52
seank842 <seanknox@Seans-MacBook-Pro.local>
2017-03-02 12:01:55 +0000

    tsting

--- Scripts/js_ajax/itemGrid.js
+++ Scripts/js_ajax/itemGrid.js
@@ -90,7 +90,7 @@
                         '<br> &nbsp;<i class="glyphicon glyphicon-xbt"></i>: ' +
                         data.items.item[label++].Value
                 });
-        } else if ($("#shop").length) {var element = $("#inven_x5F_slot_x5F_"+(label+1)).append($("<foreignObject></foreignObject>").append($("<div></div>").addClass("item")
+        } else if ($("#shop").length) {var element = $("#inven_x5F_slot_x5F_"+(label+1)).append($("<foreignObject></foreignObject>").create($("<div></div>").addClass("item")
                 .attr({
                     id: data.items.item[label].ItemID,
                     'data-toggle': "tooltip",
