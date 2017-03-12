0e153a77a6ed026c703083af07b7ed37b8defb7b
Sean Knox <seank842@live.co.uk>
2017-03-12 17:14:37 +0000

    fixed bugs

--- Scripts/js_ajax/charaStatUps.js
+++ Scripts/js_ajax/charaStatUps.js
@@ -80,73 +80,62 @@ function loadStats(stats, tempDiv) {
     setCard(tempDiv);
 }
 
-function addStat(tempid, stat) {
-    console.log("entered");
+function addStat(stat, charidN) {
     console.log(stat);
-    console.log(tempid);
     switch (stat) {
         case "str":
-            var stre = $(tempid).find("." + stat).attr('id');
-            if (!uploadStat(stre, 2)) {
-                toastr['error']("There was a problem uploading the change please try again.", "Stat Buffing");
-            } else {
-                stre++
-                var element = $(tempid).find("." + stat);
-                element.attr('id', stre);
-                element.html(stre);
-            }
+            var stre = $("#stats_" + charidN).find(".stre").attr('id');
+            uploadStat(stre, 2, charidN);
+            stre++
+            var element = $("#stats_" + charidN).find(".stre");
+            element.attr('id', stre);
+            element.html(stre);
             break;
         case "agi":
-            var agil = $(tempid).find("." + stat).attr('id');
-            if (!uploadStat(agil, 3)) {
-                toastr['error']("There was a problem uploading the change please try again.", "Stat Buffing");
-            } else {
-                agil++;
-                var element = $(tempid).find("." + stat);
-                element.attr('id', agil);
-                element.html(agil);
-            }
+            var agil = $("#stats_" + charidN).find(".agil").attr('id');
+            uploadStat(agil, 3, charidN);
+            agil++;
+            var element = $("#stats_" + charidN).find(".agil");
+            element.attr('id', agil);
+            element.html(agil);
             break;
         case "stam":
-            var sta = $(tempid).find("." + stat).attr('id');
-            if (!uploadStat(sta, 4)) {
-                toastr['error']("There was a problem uploading the change please try again.", "Stat Buffing");
-            } else {
-                sta++;
-                var element = $(tempid).find("." + stat);
-                element.attr('id', sta);
-                element.html(sta);
-            }
+            var sta = $("#stats_" + charidN).find(".sta").attr('id');
+            uploadStat(sta, 4, charidN);
+            sta++;
+            var element = $("#stats_" + charidN).find(".sta");
+            element.attr('id', sta);
+            element.html(sta);
             break;
         case "magi":
-            var magic = $(tempid).find("." + stat).attr('id');
-            if (!uploadStat(magic, 5)) {
-                toastr['error']("There was a problem uploading the change please try again.", "Stat Buffing");
-            } else {
-                magic++;
-                var element = $(tempid).find("." + stat);
-                element.attr('id', magic);
-                element.html(magic);
-            }
+            var magic = $("#stats_" + charidN).find(".magic").attr('id');
+            uploadStat(magic, 5, charidN);
+            magic++;
+            var element = $("#stats_" + charidN).find(".magic");
+            element.attr('id', magic);
+            element.html(magic);
             break;
         default:
             console.log("error");
     }
 }
 
-function uploadStat(charid, statid) {
-    postUD = { CharID: charid, StatID: statid, Token: localStorage.getItem('userToken') };
+function uploadStat(charid, statid, charidN) {
+    postUD = { CharID: charidN, StatID: statid, Token: localStorage.getItem('userToken') };
+    console.log(postUD);
     $.ajax({
         async: true,
         type: "POST",
         url: "api/chara/buff.php",
-        data: postD,
+        data: postUD,
         cache: false,
         processData: true,
         success: function (data) {
             var results = JSON.parse(data);
+            console.log(results);
             if (results.success) {
                 toastr['success']("Buff applied!", "Buffing")
+                return true;
             } else {
                 toastr['error']("Unable to apply buff!", "Buffing error")
             }
